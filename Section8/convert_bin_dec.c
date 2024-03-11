2e6e5931deda49bc5494771a06836736885f65df
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-11 09:19:45 +0100

    section 8 convertion

--- Section8/convert_bin_dec.c
+++ Section8/convert_bin_dec.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+for some reason function DecToBin() works correctly only if 
+I comment on lines of the previous code.
+In other case, the output is : 5 in decimal = 4294967400 in binary
+Mystery
+*/
+
 int BinToDec(long long n);
+long long DecToBin(int n);
 
 int main(){
     
     long long bi;
-    int result =0, de;
-    printf("Enter a binary number: ");
-    scanf("%lld", &bi);
+    long long resultB;
+    int result =0, de =0;
 
-    result = BinToDec(bi);
-    printf("%lld in binary = %d in decimal\n", bi, result);
+    // printf("Enter a binary number: ");
+    // scanf("%lld", &bi);
+    // result = BinToDec(bi);
+    // printf("%lld in binary = %d in decimal\n", bi, result);
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &de);
+    resultB = DecToBin(de);
+    printf("%d in decimal = %lld in binary\n", de, resultB);
 
 
     return 0;
@@ -28,4 +42,16 @@ int BinToDec(long long n){
     }
 
     return result;
+}
+long long DecToBin(int n){
+    long long result;
+    int rem=0, i=1;
+    while ( n != 0){
+        rem = n%2;
+        n = n/2;
+        result += rem * i;
+        i = i*10;
+    }
+    return result;
+
 }
