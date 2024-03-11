79fe727ccbd4b87d770bdc87d72537f25eefc7d3
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-11 11:10:15 +0100

    chal print

--- Section8/chal.c
+++ Section8/chal.c
@@ -10,12 +10,12 @@ int main(){
     scanf("%d", &num1);
     printf("Enter the second decimal number: ");
     scanf("%d", &num2);
-    int decimal_display = 0;
+    int decimal_left_shift = 0;
     long long complement1=0, complement2=0;
     long long and1=0, and2=0;
     long long or1=0, or2=0;
     long long exor1=0, exor2=0;
-    long long shift=0;
+    long long shift_left=0;
     Bnum1 = DecToBin(num1);
     Bnum2 = DecToBin(num2);
     complement1 = DecToBin(~num1);
@@ -26,9 +26,15 @@ int main(){
     or2 = DecToBin(num2|num1);
     exor1 = DecToBin(num1^num2);
     exor2 = DecToBin(num2|num1);
+    decimal_left_shift = num1 << 2;
+    shift_left = DecToBin(decimal_left_shift);
 
-
-
+    //printing
+    printf("The result of applying the ~ operator on number %d (%lld) is %lld\n", num1, Bnum1, complement1);
+    printf("The result of applying the ~ operator on number %d (%lld) is %lld\n", num2, Bnum2, complement2);
+    printf("The result of applying the & operator on numbers %d (%lld) and %d (%lld)  is %lld\n", num1, Bnum1,num2, Bnum2, and1);
+    printf("The result of applying the & operator on numbers %d (%lld) and %d (%lld)  is %lld\n", num2, Bnum2,num1, Bnum1, and2);
+    
 
     return 0;
 }
