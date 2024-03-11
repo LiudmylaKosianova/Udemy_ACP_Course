1a90944bbbb1089c7fcc823cb69385214ea4c1bd
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-11 08:52:13 +0100

    section 8

--- Section8/convert_bin_dec.c
+++ Section8/convert_bin_dec.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 
-int BiToDe(long long n);
+int BinToDec(long long n);
 
 int main(){
-
-    int de;
+    
     long long bi;
-    int result =0;
+    int result =0, de;
     printf("Enter a binary number: ");
     scanf("%lld", &bi);
 
-    result = BiToDe(bi);
+    result = BinToDec(bi);
     printf("%lld in binary = %d in decimal\n", bi, result);
 
 
     return 0;
 }
 
-int BiToDe(long long n){
+int BinToDec(long long n){
     int result = 0, i = 0, rem = 0;
     while (n != 0){
-        
+        rem = n%10; //gets the most right digit
+        n = n/10;
+        result += rem * pow(2, i);
+        i++;
+
     }
 
     return result;
