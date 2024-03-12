56331800701f7e25772a6e5a2dcce90609515cee
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-12 07:35:34 +0100

    section 8

--- Section8/chal3.c
+++ Section8/chal3.c
@@ -19,15 +19,41 @@
 const char *colours[8] = {"black", "red", "green", "yellow", "blue", "magenta","cyan", "white"};
 
 struct onscreen_box{
-    unsigned int: 16;
+
     unsigned int opaque:1;//1-opeque, 0-transparen
-    unsigned int colour:3;
+    unsigned int fillColour:3;
+    unsigned int:4; //padding
     unsigned int borderShown:1;//1-shown, 0-not shown
-    unsigned int colour:3;
+    unsigned int borderColour:3;
     unsigned int borderStyle:2;
+    unsigned int:2;//padding
 };
+
+void show_settings(const struct onscreen_box *ptr);
 
 int main(){
 
+    struct onscreen_box box = {1,YELLOW,1,GREEN,DASHED};
+    printf("The initial settings are:\n");
+    show_settings(&box);
+    box.borderColour = BLACK;
+    show_settings(&box);
+
     return 0;
+}
+
+void show_settings(const struct onscreen_box *ptr){
+    printf("Box is %s\n",ptr->opaque == 1 ? "opeque":"transparent");
+    printf("The fill colour is %s\n", colours[ptr->fillColour]);
+    printf("Border is %s\n", ptr->borderShown == 1 ? "shown":"not shown");
+    printf("The border colour is %s\n", colours[ptr->borderColour]);
+    printf("The border style is ");
+
+    switch (ptr->borderStyle){
+        case SOLID  :printf("solid\n"); break;
+        case DOTTED :printf("dotted\n"); break;
+        case DASHED :printf("dashed\n"); break;
+        default     :printf("unknown\n");
+    }
+    printf("\n");
 }
