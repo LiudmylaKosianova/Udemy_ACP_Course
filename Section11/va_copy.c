3081cf394aa60544ec54d9091f7418365427a302
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-15 13:17:18 +0100

    va_copy section 11

---
+++ Section11/va_copy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
+
+double sample(int count, ...);//... is called ellipsis
+
+int main(){
+
+    return 0;
+}
+double sample(int count, ...){
+
+    return 0.0;
+}
--- Section11/varFunc.c
+++ Section11/varFunc.c
@@ -9,13 +9,28 @@ int main(){
     int num1 = 6, num2 = 5;
     long num3 = 12L, num4 = 20L;
 
-    
+    printf("Average = %.2lf\n", average(v1, 3.5, v2, 4.5, 0.0));
+    printf("Average = %.2lf\n", average(1.0, 2.0, 0.0));
+    printf("Average = %.2lf\n", average((double)num2, v2, (double)num1, (double)num4, (double)num3, 0.0 ));
+
 
 
     return 0;
 }
 
 double average(double v1, double v2, ...){
+    va_list parg; //pointer for variable argument list
+    double sum = v1 + v2;
+    double value = 0.0;//will be used to store the arguments' values as they will be obtained one by one
+    int count = 2;//there are at least two mandatory arguments
 
-    return 0.0;
+    va_start(parg,v2);//takes the list and the last fixed argument
+
+    while(  (value = va_arg(parg, double)) != 0.0) {//assume, that the last argument is 0.0
+        sum += value;
+        count++;
+    }
+
+    va_end(parg);
+    return sum / count;
 }
