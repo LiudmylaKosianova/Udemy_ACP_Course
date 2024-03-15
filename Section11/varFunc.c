960220bd4a7f43c763bfd60df16dbb025f884f41
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-15 12:55:01 +0100

    varFunc section 11

---
+++ Section11/varFunc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+double average(double v1, double v2, ...);
+
+int main(){
+
+    double v1 = 10.5, v2 = 2.5;
+    int num1 = 6, num2 = 5;
+    long num3 = 12L, num4 = 20L;
+
+    
+
+
+    return 0;
+}
+
+double average(double v1, double v2, ...){
+
+    return 0.0;
+}
