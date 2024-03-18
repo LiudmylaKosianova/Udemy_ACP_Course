877902afc7937f177a24cf4e45f742bfccec8221
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-18 10:10:22 +0100

    macro 2

--- Section14/macro.c
+++ Section14/macro.c
@@ -7,11 +7,18 @@
     printf("value a = %d\n",a);\
     printf("value b = %d\n", b);
 
+#define CIRCLE_A(X) ((PI)*(X)*(X))
+
+#define UpTo(i,n) for((i)=0;(i)<(n); (i)++)//not syntactic -> using this macro your C code doesn't look like C
+
 int main(){
 
     int a = 224;
     int b = 87;
     PRNT(a,b);
+
+    int area = CIRCLE_A(4);
+    printf("%d is area\n", area);
 
     return 0;
 }
