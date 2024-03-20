3a4ca73277a11fd92fcadfc502e63363261a2ba5
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-20 13:56:19 +0100

    section 16

--- Section16/doubleP.c
+++ Section16/doubleP.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void sayHello(char **ptr){
+    *ptr = malloc(sizeof(char) * 255);
+    strcpy(*ptr, "Hello there");
+}
 
 
 int main(){
@@ -16,7 +23,15 @@ int main(){
     printf("%p address of p1\n", (void*) &p1);
     printf("%p address of p2\n",(void*) &p2);
 
+    printf("%d value of v\n", v);
+    printf("%p value of p1\n",(void*) p1);
+    printf("%p value of p2\n",(void*) p2);
 
+    printf("**p2 = %d\n", **p2);//the same as *(*p2)
 
+    char *ptr = NULL;
+    sayHello(&ptr); //this will pass a double pointer
+    printf("%s\n", ptr);
+    free(ptr);
     return 0;
 }
