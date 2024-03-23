7c6a6bb336a3ed39fe9bf82f51b49b93c76ea836
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-23 10:48:42 +0100

    convert functions

---
+++ Section18/conv.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+    /*atoi() - coverts to integer*/
+    char a[11] = "100";
+    int value = atoi(a);
+    printf("value=%d\n",value);
+
+    /*atof() - converts to float */
+    float value1 = atof(a);
+    printf("value1=%.3lf\n",value1);
+
+    /*atol()*/
+    long int value2 = atol(a);
+    printf("value2=%ld\n",value2);
+
+    /*sprintf()*/
+    sprintf(a,"%d\n", 45);
+    printf("%s\n", a);
+
+    /*strtod()*/
+    char aa[] = "123.45zyyhoo";
+    char *end;
+    double v = strtod(aa, &end); //&end - because it takes double pointer
+    printf("v=%lf\n", v);
+
+    /*strtof()*/
+    char aaa[] = "23.55 7.9";
+    char *end1 = NULL;
+    float val1 = strtof(aaa, &end1);
+    float val2 = strtof(end1, NULL);
+
+    printf("val1=%f and val2=%f\n", val1, val2);
+
+    return 0;
+}
