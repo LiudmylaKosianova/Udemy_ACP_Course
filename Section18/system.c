0bd75987eaa0d571e7b5eb13b6c7c4b00f1d93c1
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-23 18:25:31 +0100

    chal 1 section18

--- Section18/system.c
+++ Section18/system.c
@@ -1,11 +1,18 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 int main(){
 
     char command[70];
     strcpy(command, "ls -la");
     system(command);
+
+    /*getenv()*/
+    char *str = NULL;
+    str = getenv("HOME");
+    printf("%s \n", str);
+
 
 
     return 0;
