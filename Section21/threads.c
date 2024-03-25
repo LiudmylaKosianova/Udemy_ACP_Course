2f028bd1f74453d1fc4aabe85dbc73258aa34e9d
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-03-25 08:16:00 +0100

    thread and one argument

--- Section21/threads.c
+++ Section21/threads.c
@@ -5,11 +5,31 @@ void *helloFun(){
     printf("HelloFun\n");
     return NULL;
 }
+void *print_thread(void *ptr){    
+    printf("%s\n", (char*)ptr);
+}
 
-int main(){
+int main(int argc, char *arg[]){
 
-    pthread_t redSofa;
-    pthread_create(&redSofa, NULL, helloFun, NULL );
-    pthread_exit (NULL);
+    // pthread_t redSofa;
+    // pthread_create(&redSofa, NULL, helloFun, NULL );    
+    // pthread_join(redSofa, NULL);
+    // pthread_exit (NULL);
+
+    pthread_t thr1, thr2;
+    char *mes1 = "thread 1";
+    char *mes2 = "thread 2";
+    int r1, r2;
+    r1 = pthread_create(&thr1, NULL, print_thread, (void*)mes1);
+    r2 = pthread_create(&thr2, NULL, print_thread, (void*)mes2);
+    pthread_join(thr1, NULL);
+    pthread_join(thr2, NULL);
+
+    printf("thr1 returns %d\n", r1);
+    printf("thr2 returns %d\n", r2);
+
+    pthread_exit(NULL);
+
+
     return 0;
 }
